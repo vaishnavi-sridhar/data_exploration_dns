1b969b5775f2e1f57c9f9192807d35387fabbea8
Blanca Muñoz Perez <blancamunozperez@cu-genvpn-comp-10.180.13.103.int.colorado.edu>
2022-02-28 16:53:15 -0700

    graph 4 finished

--- graph.py
+++ graph.py
@@ -23,34 +23,40 @@ def plot_graph(inputfile, label_plot, x_axis, y_axis, title_plot):
 
 
 def plot_graph4():
-    content_as_dict = json.load(open('Graph4.txt', 'r'))
+    content_as_dict = json.load(open('Graph4-old.txt', 'r'))
 
     labels = list(content_as_dict.keys())
-    top1_domain =[]
-    top2_domain = []
+
+    top_domains = [[],[],[],[],[]]
     for inner_dict in content_as_dict.values():
+        sorted_tuples = sorted(inner_dict.items(), key=lambda item: item[1])
+        print(sorted_tuples)
+        inner_dict = {k: v for k, v in sorted_tuples}
         for index, (key,value) in enumerate(inner_dict.items()):
-            if index == 0:
-                top1_domain.append(inner_dict[key])
-            else:
-                top2_domain.append(inner_dict[key])
+            if index <= 4:        
+                top_domains[index].append(inner_dict[key])
+
 
     x = np.arange(len(labels))  # the label locations
-    width = 0.35  # the width of the bars
-
+    width = 0.1  # the width of the bars
+    print("HeyHEy",content_as_dict)
     fig, ax = plt.subplots()
-    rects1 = ax.bar(x - width / 2, top1_domain, width, label='Top1 domain')
-    rects2 = ax.bar(x + width / 2, top2_domain, width, label='Top2 domain')
+    rects = []
+    rects.append(ax.bar(x - width / 2, top_domains[0], width, label='Top1 domain'))
+    rects.append(ax.bar(x + width / 2, top_domains[1], width, label='Top2 domain'))
+    rects.append(ax.bar(x + 1.5*width, top_domains[2], width, label='Top3 domain'))
+    rects.append(ax.bar(x + 2.5*width, top_domains[3], width, label='Top4 domain'))
+    rects.append(ax.bar(x + 3.5*width, top_domains[4], width, label='Top5 domain'))
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
     ax.set_ylabel('Domain query count')
     ax.set_xlabel('Time of day')
-    ax.set_title('Top 2 domains for different times of the day')
+    ax.set_title('Top 5 domains for different times of the day')
     ax.set_xticks(x, labels)
     ax.legend()
+    for i in range(5):
+        ax.bar_label(rects[i], padding=10)
 
-    ax.bar_label(rects1, padding=3)
-    ax.bar_label(rects2, padding=3)
     fig.autofmt_xdate()
     fig.tight_layout()
 
@@ -107,5 +113,5 @@ def autolabel(rects,ax,values):
 
 #plot_graph('Graph1-old.txt', 'DNS Record count', 'Times of day', 'No. of DNS records (normalized)', 'DNS record count (vs) Time of the day')
 #plot_graph('Graph2.txt', 'Record Count', 'Query Type', 'Query Type Count', 'Query Type (vs) Count')
-plot_graph3()
-#plot_graph4()
+#plot_graph3()
+plot_graph4()
